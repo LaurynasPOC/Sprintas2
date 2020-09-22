d959643c6ac6d87ed98bd9dd22c499d7e203acc7
LaurynasPOC <laurynas.pociunas@gmail.com>
2020-09-22 22:23:20 +0300

    Sprintas2

--- src/Sprintas2/Main.java
+++ src/Sprintas2/Main.java
@@ -4,47 +4,53 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter employee name: ");
-        String name = sc.nextLine();
-        System.out.println("Enter employee last name: ");
-        String lastName = sc.nextLine();
-        System.out.println("Enter employee salary: ");
-        int salary = sc.nextInt();
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter employee name: ");
+            String name = sc.nextLine();
+            System.out.println("Enter employee last name: ");
+            String lastName = sc.nextLine();
+            System.out.println("Enter employee salary: ");
+            int salary = sc.nextInt();
+            sc.close();
 
-        EmployeeInfo personInfo = new EmployeeInfo();
-        personInfo.setName(name);
-        personInfo.setLastName(lastName);
-        personInfo.setSalary(salary);
+            EmployeeInfo personInfo = new EmployeeInfo();
+            personInfo.setName(name);
+            personInfo.setLastName(lastName);
+            personInfo.setSalary(salary);
 
-        File employeesFile = new File("./data/writtingToFiles.csv");
-        try {
+            File employeesFile = new File("./data/writtingToFiles.csv");
+            try {
 
-            FileWriter write = new FileWriter(employeesFile, true);
-            BufferedWriter ewr = new BufferedWriter(write);
+                FileWriter write = new FileWriter(employeesFile, true);
+                BufferedWriter ewr = new BufferedWriter(write);
+                ewr.write("Vardas |" + " Pavardė |" +"Atlyginimas" + "\n");
+                ewr.write(personInfo.getName()+" | "+personInfo.getLastName()+ " | " +personInfo.getSalary()+"eur"+"\n");
+                ewr.write("\n");
+                ewr.close();
 
-            ewr.write("Darbuotojo vardas: " + personInfo.getName()+ "\n");
-            ewr.write("Darbuotojo pavardė: " + personInfo.getLastName()+ "\n");
-            ewr.write("Darbuotojo atlyginimas: " + personInfo.getSalary()+ " eur" + "\n");
-            ewr.close();
+                FileReader reader = new FileReader(employeesFile);
+                BufferedReader erd = new BufferedReader(reader);
+                String fileLines = erd.readLine();
 
-            FileReader reader = new FileReader(employeesFile);
-            BufferedReader erd = new BufferedReader(reader);
-            String fileLines = erd.readLine();
-            while (fileLines != null){
 
-                String[] splitString = fileLines.split(" ");
-                fileLines = erd.readLine();
-                System.out.println(Arrays.toString(splitString));
+                while (fileLines != null){
 
+                    String[] splitString = fileLines.split(" ");
+                    fileLines = erd.readLine();
+                    System.out.println(Arrays.toString(splitString));
+
+                }
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
             }
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
 
+        }
     }
-}
+
+
+
+
